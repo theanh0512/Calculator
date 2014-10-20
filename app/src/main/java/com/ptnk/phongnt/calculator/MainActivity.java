effef94ea9505064df41d379281212173e51cb88
Phong Nguyen <phongnt.ptnk@gmail.com>
2014-10-21 01:21:32 +0800

    new API

--- app/src/main/java/com/ptnk/phongnt/calculator/MainActivity.java
+++ app/src/main/java/com/ptnk/phongnt/calculator/MainActivity.java
@@ -1,4 +1,4 @@
-package com.ntu.phongnt.calculator;
+package com.ptnk.phongnt.calculator;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -8,13 +8,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.ptnk.phongnt.calculator.R;
+
+import net.sourceforge.jeval.EvaluationException;
+import net.sourceforge.jeval.Evaluator;
+
 
 public class MainActivity extends Activity {
-    float ans;
-    float current;
+    String ans;
+    String current;
+    Evaluator evaluator;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        evaluator = new Evaluator();
+        ans = "0";
         setContentView(R.layout.activity_main);
         clearDisplay();
     }
@@ -66,24 +74,42 @@ public class MainActivity extends Activity {
         return Float.parseFloat(string);
     }
 
-    public void onNumberClicked(View view){
+    public void onSymbolClicked(View view){
         String currentText = getDisplayText();
         Button b = (Button)view;
         String buttonText = b.getText().toString();
         if(! currentText.equals("0"))
             setDisplayText(currentText+buttonText);
-        else if (! buttonText.equals("0"))
+        else
             setDisplayText(buttonText);
     }
 
-    public void binaryOpsClicked(View view){
+    public void onOperatorClicked(View view){
         String currentText = getDisplayText();
         Button b = (Button)view;
         String buttonText = b.getText().toString();
         if(! currentText.equals("0"))
             setDisplayText(currentText+buttonText);
-        else if (! buttonText.equals("0"))
-            setDisplayText(buttonText);
+        else
+            setDisplayText("ans"+buttonText);
+    }
+
+    public void onEvaluated(View view){
+        String expression = getDisplayText().replace("ans",ans);
+        try{
+            ans = evaluator.evaluate(expression);
+            showResult(ans);
+            clearDisplay();
+        }
+        catch (EvaluationException e){
+            showResult("Error");
+        }
+
+    }
+
+    public void showResult(String result){
+        TextView display = (TextView) findViewById(R.id.display_result);
+        display.setText(result);
     }
 
     public void onCleared(View view){
